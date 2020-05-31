fcb613115a93708a0f80bb6972cce1f50791740d
Preeth Kanamangala <preeth.kanamangala@gmail.com>
2020-05-31 23:42:02 +0000

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	flash = require("connect-flash"),
+	mongoose  = require("mongoose"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	Campground = require("./models/campgrounds"),
+	methodOverride = require("method-override"),
+	User = require("./models/user"),
+	Comment = require("./models/comment"),
+	seedDB = require("./seeds");
+
+var commentRoutes = require("./routes/comments"),
+	campgroundRoutes = require("./routes/campgrounds"),
+	indexRoutes = require("./routes/index");
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB();
+
+//passport config
+app.use(require("express-session")({
+	secret: "this is a web development class",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// currentUser is available for every route
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+})
+
+app.use("/", indexRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function() {
+	console.log("starting YelpCamp server...");
+});
